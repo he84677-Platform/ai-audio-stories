0d2724b3d5d1e6644612ea058fc04a337e6bd3d4
Travis <he84677@gmail.com>
2026-07-21 14:57:15 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { supabase } from "@/lib/supabase";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const { data: stories, error } = await supabase
     .from("stories")
